5192d5118e0d605b60aa033be14f130e7b5fc4fb
Your Name <your@example.com>
2026-04-17 14:13:55 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask, render_template  # render_templateを追加
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    # 記事のタイトルをリストで用意
+    post_list = ["初めての投稿", "Flaskの勉強中", "CSSでデザインしてみた"]
+    return render_template("index.html", title="マイ・プログラミング日記", posts=post_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
